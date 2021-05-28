5346ca945174c9fd7d6509f2835d0f0a7000d93b
luli <luli@tuzhanai.com>
2021-05-28 19:37:43 +0800

    add an example: fiber bailout

---
+++ app/src/Bailout.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function Son() {
+  console.log("child render!");
+  return <div>Son</div>;
+}
+
+function Parent(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      count:{count}
+      {props.children}
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Parent>
+      <Son />
+    </Parent>
+  );
+}
